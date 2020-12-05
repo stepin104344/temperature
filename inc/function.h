67a01cb643e2a1f835d77f0856bd27699cf0db9c
stepin104344 <69032727+stepin104344@users.noreply.github.com>
2020-12-05 18:09:47 +0530

    Update function.h

--- inc/function.h
+++ inc/function.h
@@ -2,10 +2,10 @@
 #ifndef __FUNCTION_H__
 #define __FUNCTION_H__
 
-float do_celsius_fahrenheit(float);
-float do_kelvin_fahrenheit(float);
-float do_fahrenheit_celsius(float);
-float do_celsius_kelvin(float);
+double do_celsius_fahrenheit(double);
+double do_kelvin_fahrenheit(double);
+double do_fahrenheit_celsius(double);
+double do_celsius_kelvin(double);
 float do_kelvin_celsius(float);
 float do_fahrenheit_kelvin(float);
 
